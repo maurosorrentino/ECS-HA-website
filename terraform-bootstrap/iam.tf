0cf33120b92c05d1b40c5990435da4dcae507cc7
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-17 17:34:05 +0100

    gh actions role, needs testing

---
+++ terraform-bootstrap/iam.tf
@@ -0,0 +1,32 @@
+# TODO var
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub OIDC thumbprint
+}
+
+# TODO add environment to var and put it in name
+resource "aws_iam_role" "github_actions_role" {
+  name = "GitHubActions<ProjectName>Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:maurosorrentino/ECS-HA-failover-website/:ref:refs/heads/main"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy_attach" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
