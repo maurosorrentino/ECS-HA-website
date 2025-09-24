74a8f37dc0da52b05cc22fead08db4412c85d0cd
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-24 17:55:34 +0100

    ecr modules

--- terraform-infrastructure/ecr.tf
+++ terraform-infrastructure/ecr.tf
@@ -1,29 +1,9 @@
-resource "aws_ecr_repository" "project_name_ecr_repo" {
-  name                 = "project_name_ecr_repo"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+module "project_name_frontend_ecr_repo" {
+  source        = "./modules/ecr"
+  ecr_repo_name = "project_name_frontend_ecr_repo"
 }
 
-resource "aws_ecr_lifecycle_policy" "project_name_ecr_lc_policy" {
-    repository = aws_ecr_repository.project_name_ecr_repo.name
-
-    policy = jsonencode({
-        rules = [
-            {
-                rulePriority = 1
-                description  = "Expire untagged images if more than 4 exist"
-                selection    = {
-                    tagStatus   = "untagged"
-                    countType   = "imageCountMoreThan"
-                    countNumber = 4
-                }
-                action = {
-                    type = "expire"
-                }
-            }
-        ]
-    })
+module "name_project_backend_ecr_repo" {
+  source        = "./modules/ecr"
+  ecr_repo_name = "project_name_backend_ecr_repo"
 }
