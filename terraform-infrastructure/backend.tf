6204c8cad8ad279e5a76b843aef721f08c7bc5de
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-24 17:48:03 +0100

    removed var from s3 bucket name

--- terraform-infrastructure/backend.tf
+++ terraform-infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-department-name"
-    key            = "infrastructure/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
