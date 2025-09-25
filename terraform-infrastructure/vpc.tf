eec9238275d57e1c353a28d44e7ffc7ba44c06e6
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-25 10:51:53 +0100

    subnets, vpc, igw + empty files. needs testing

--- terraform-infrastructure/vpc.tf
+++ terraform-infrastructure/vpc.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc" "project_name_vpc" {
+    # TODO once known change cidr block to something with less IPs
     cidr_block           = "10.0.0.0/16" # over 65k IPs
     enable_dns_support   = true
     enable_dns_hostnames = true
