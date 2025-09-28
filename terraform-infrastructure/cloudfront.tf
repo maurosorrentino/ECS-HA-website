eaf4ad43538dce5d69ddad297fdd9c49256aafca
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-28 14:59:37 +0100

    cdn certificate settings changed to ACM

--- terraform-infrastructure/cloudfront.tf
+++ terraform-infrastructure/cloudfront.tf
@@ -57,7 +57,10 @@ resource "aws_cloudfront_distribution" "frontend" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = #TODO
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
   }
 
   restrictions {
