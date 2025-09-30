5bed513785c8e12ab35906cb825c1db191275429
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-30 14:15:24 +0100

    backend ecs service

--- terraform-infrastructure/ecs.tf
+++ terraform-infrastructure/ecs.tf
@@ -28,3 +28,16 @@ module "project_name_frontend_ecs_service" {
 
   depends_on = [aws_subnet.private_subnets, module.project_name_frontend_alb, aws_security_group.ecs_frontend_service_sg]
 }
+
+module "project_name_backend_ecs_service" {
+  source                = "./modules/service/ecs"
+  service_name          = "backend"
+  cluster_id            = aws_ecs_cluster.project_name_ecs_cluster.id
+  task_arn              = module.project_name_backend_ecs_task.ecs_task_arn
+  subnets_ids           = [ for name, subnet in aws_subnet.private_subnets : subnet.id if contains(name, "backend") ]
+  security_groups_ids   = [aws_security_group.ecs_backend_service_sg.id]
+  alb_target_group_arn  = module.project_name_backend_alb.target_group_arn
+
+  depends_on = [aws_subnet.private_subnets, module.project_name_backend_alb, aws_security_group.ecs_backend_service_sg]
+  
+}
