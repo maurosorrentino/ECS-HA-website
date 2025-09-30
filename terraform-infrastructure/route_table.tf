631039a4e696f12ff65349c48531330c5e46ab21
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-30 14:52:44 +0100

    terraform fmt

--- terraform-infrastructure/route_table.tf
+++ terraform-infrastructure/route_table.tf
@@ -14,7 +14,7 @@ resource "aws_route_table" "project_name_public_rt" {
 }
 
 resource "aws_route_table_association" "public" {
-  for_each = aws_subnet.project_name_public_subnets
+  for_each       = aws_subnet.project_name_public_subnets
   subnet_id      = each.value.id
   route_table_id = aws_route_table.project_name_public_rt.id
 }
