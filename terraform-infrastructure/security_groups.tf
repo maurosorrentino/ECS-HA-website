af7482c69b54668e938d47a8c81fa141043f5abf
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-30 14:27:35 +0100

    sg missing rules fix

--- terraform-infrastructure/security_groups.tf
+++ terraform-infrastructure/security_groups.tf
@@ -3,10 +3,12 @@ data "aws_ip_ranges" "cloudfront" {
   regions  = ["GLOBAL"]
 }
 
-resource "aws_security_group" "project_name_ecs_frontend_service_sg" {
+resource "aws_security_group" "project_name_ecs_frontend_service_sg" { #done
   name        = "${var.project_name}-ecs-frontend-service-sg"
   description = "ECS service SG: ALB in -> ECS -> private ALB out"
   vpc_id      = aws_vpc.project_name_vpc.id
+
+  depends_on = [aws_vpc.project_name_vpc]
 }
 
 resource "aws_security_group" "project_name_frontend_alb_sg" {
@@ -17,7 +19,7 @@ resource "aws_security_group" "project_name_frontend_alb_sg" {
   depends_on = [aws_vpc.project_name_vpc]
 }
 
-resource "aws_security_group" "project_name_backend_alb_sg" {
+resource "aws_security_group" "project_name_backend_alb_sg" { #done
   name        = "${var.project_name}-backend-alb-sg"
   description = "Allow HTTP from ECS service to backend ALB"
   vpc_id      = aws_vpc.project_name_vpc.id
@@ -25,9 +27,17 @@ resource "aws_security_group" "project_name_backend_alb_sg" {
   depends_on = [aws_vpc.project_name_vpc]
 }
 
-resource "aws_security_group" "project_name_rds_sg" {
+resource "aws_security_group" "project_name_rds_sg" { # done
   name        = "${var.project_name}-rds-sg"
   description = "RDS security group"
+  vpc_id      = aws_vpc.project_name_vpc.id
+
+  depends_on = [aws_vpc.project_name_vpc]
+}
+
+resource "aws_security_group" "project_name_ecs_backend_service_sg" { #done
+  name        = "${var.project_name}-ecs-backend-service-sg"
+  description = "backend ECS service to RDS"
   vpc_id      = aws_vpc.project_name_vpc.id
 
   depends_on = [aws_vpc.project_name_vpc]
@@ -51,12 +61,6 @@ resource "aws_security_group_rule" "project_name_rds_sg_egress" {
   protocol          = "-1"
   security_group_id = aws_security_group.project_name_rds_sg.id
   cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "project_name_ecs_backend_service_sg" {
-  name        = "${var.project_name}-ecs-backend-service-sg"
-  description = "backend ECS service to RDS"
-  vpc_id      = aws_vpc.project_name_vpc.id
 }
 
 resource "aws_security_group_rule" "project_name_ecs_backend_service_sg_ingress" {
@@ -100,13 +104,13 @@ resource "aws_security_group_rule" "project_name_backend_alb_sg_egress" {
 }
 
 resource "aws_security_group_rule" "project_name_ecs_frontend_service_ingress" {
-  description       = "Allow HTTPS from CloudFront to ALB"
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.project_name_ecs_frontend_alb_sg.id
-  cidr_blocks       = [for range in data.aws_ip_ranges.cloudfront.cidr_blocks : range]
+  description              = "Allow HTTPS from CloudFront to ALB"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.project_name_ecs_frontend_alb_sg.id
+  source_security_group_id = aws_security_group.project_name_frontend_alb_sg.id
 }
 
 resource "aws_security_group_rule" "project_name_ecs_frontend_service_egress" {
@@ -119,22 +123,22 @@ resource "aws_security_group_rule" "project_name_ecs_frontend_service_egress" {
   source_security_group_id = [aws_security_group.project_name_ecs_frontend_service_sg.id]
 }
 
-resource "aws_security_group_rule" "project_name_ecs_frontend_service_ingress" {
-  description              = "Allow HTTP from public ALB"
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.project_name_ecs_frontend_service_sg.id
-  source_security_group_id = aws_security_group.project_name_frontend_alb_sg.id
+resource "aws_security_group_rule" "project_name_frontend_alb_sg_ingress" {
+  description       = "Allow HTTP from public ALB"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.project_name_frontend_alb_sg.id
+  cidr_blocks       = [for range in data.aws_ip_ranges.cloudfront.cidr_blocks : range]
 }
 
-resource "aws_security_group_rule" "project_name_ecs_frontend_service_egress" {
+resource "aws_security_group_rule" "project_name_frontend_alb_sg_egress" {
   description              = "Allow ECS to send HTTP to private ALB"
   type                     = "egress"
   from_port                = 80
   to_port                  = 80
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.project_name_ecs_frontend_service_sg.id
-  source_security_group_id = aws_security_group.project_name_backend_alb_sg.id
+  security_group_id        = aws_security_group.project_name_frontend_alb_sg.id
+  source_security_group_id = aws_security_group.project_name_frontend_ecs_service_sg.id
 }
