331dbe58d90a90fe977a0b3d267a9954360648d7
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-30 14:58:05 +0100

    Reference to undeclared resource bug fix

--- terraform-infrastructure/ecs.tf
+++ terraform-infrastructure/ecs.tf
@@ -23,10 +23,10 @@ module "project_name_frontend_ecs_service" {
   cluster_id           = aws_ecs_cluster.project_name_ecs_cluster.id
   task_arn             = module.project_name_frontend_ecs_task.ecs_task_arn
   subnets_ids          = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if contains(name, "frontend")]
-  security_groups_ids  = [aws_security_group.ecs_frontend_service_sg.id]
+  security_groups_ids  = [aws_security_group.project_name_ecs_frontend_service_sg.id]
   alb_target_group_arn = module.project_name_frontend_alb.target_group_arn
 
-  depends_on = [aws_subnet.project_name_private_subnets, module.project_name_frontend_alb, aws_security_group.ecs_frontend_service_sg]
+  depends_on = [aws_subnet.project_name_private_subnets, module.project_name_frontend_alb, aws_security_group.project_name_ecs_frontend_service_sg]
 }
 
 module "project_name_backend_ecs_service" {
@@ -35,9 +35,9 @@ module "project_name_backend_ecs_service" {
   cluster_id           = aws_ecs_cluster.project_name_ecs_cluster.id
   task_arn             = module.project_name_backend_ecs_task.ecs_task_arn
   subnets_ids          = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if contains(name, "backend")]
-  security_groups_ids  = [aws_security_group.ecs_backend_service_sg.id]
+  security_groups_ids  = [aws_security_group.project_name_ecs_backend_service_sg.id]
   alb_target_group_arn = module.project_name_backend_alb.target_group_arn
 
-  depends_on = [aws_subnet.project_name_private_subnets, module.project_name_backend_alb, aws_security_group.ecs_backend_service_sg]
+  depends_on = [aws_subnet.project_name_private_subnets, module.project_name_backend_alb, aws_security_group.project_name_ecs_backend_service_sg]
 
 }
