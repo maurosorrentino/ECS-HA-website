10d85965c349f29612d398155241ea557813a077
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-30 14:46:46 +0100

    invalid index bug fix

--- terraform-infrastructure/route53.tf
+++ terraform-infrastructure/route53.tf
@@ -3,10 +3,14 @@ resource "aws_route53_zone" "project_name_zone" {
 }
 
 resource "aws_route53_record" "project_name_cdn_cert_validation" {
-  name    = aws_acm_certificate.project_name_cdn_cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.project_name_cdn_cert.domain_validation_options[0].resource_record_type
+  for_each = {
+    for dvo in aws_acm_certificate.project_name_cdn_cert.domain_validation_options : dvo.domain_name => dvo
+  }
+
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
   zone_id = aws_route53_zone.project_name_zone.zone_id
-  records = [aws_acm_certificate.project_name_cdn_cert.domain_validation_options[0].resource_record_value]
+  records = [each.value.resource_record_value]
   ttl     = 300
 
   depends_on = [aws_acm_certificate.project_name_cdn_cert]
