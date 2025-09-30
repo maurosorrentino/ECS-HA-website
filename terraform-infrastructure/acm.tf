37dd8b884b5364e22f4deb87febc353fdf7a5267
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-30 14:33:12 +0100

    route53 record not declared fix

--- terraform-infrastructure/acm.tf
+++ terraform-infrastructure/acm.tf
@@ -10,4 +10,6 @@ resource "aws_acm_certificate" "project_name_cdn_cert" {
 resource "aws_acm_certificate_validation" "project_name_cdn_cert_validation" {
   certificate_arn         = aws_acm_certificate.project_name_cdn_cert.arn
   validation_record_fqdns = [aws_route53_record.project_name_cdn_cert_validation.fqdn]
+
+  depends_on = [aws_route53_record.project_name_cdn_cert_validation]
 }
