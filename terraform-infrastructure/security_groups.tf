b3338feeb8c709d40e300fe9c0ed8a7fa2e138dc
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-09-30 11:55:55 +0100

    sg done but need to add aws_security_group_rule they cannot be put in order

--- terraform-infrastructure/security_groups.tf
+++ terraform-infrastructure/security_groups.tf
@@ -3,29 +3,6 @@ data "aws_ip_ranges" "cloudfront" {
   regions  = ["GLOBAL"]
 }
 
-resource "aws_security_group" "frontend_alb_sg" {
-  name        = "${var.project_name}-frontend-alb-sg"
-  description = "Allow HTTPS from CloudFront to ALB"
-  vpc_id      = aws_vpc.project_name_vpc.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [for range in data.aws_ip_ranges.cloudfront.cidr_blocks : range]
-  }
-
-  egress {
-    description     = "Allow ALB to ECS only on port 80"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_sg.id]
-  }
-
-  depends_on = [aws_vpc.project_name_vpc, aws_cloudfront_distribution.project_name_cdn] #TODO
-}
-
 resource "aws_security_group" "ecs_frontend_service_sg" {
   name        = "${var.project_name}-ecs-frontend-service-sg"
   description = "ECS service SG: ALB in -> ECS -> private ALB out"
@@ -44,6 +21,96 @@ resource "aws_security_group" "ecs_frontend_service_sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = #TODO
+    security_groups = [aws_security_group.backend_alb_sg.id]
+  }
+}
+
+resource "aws_security_group" "frontend_alb_sg" {
+  name        = "${var.project_name}-frontend-alb-sg"
+  description = "Allow HTTPS from CloudFront to ALB"
+  vpc_id      = aws_vpc.project_name_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [for range in data.aws_ip_ranges.cloudfront.cidr_blocks : range]
+  }
+
+  egress {
+    description     = "Allow ALB to ECS only on port 80"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_frontend_service_sg.id]
+  }
+
+  depends_on = [aws_vpc.project_name_vpc, aws_cloudfront_distribution.project_name_cdn] #TODO
+}
+
+resource "aws_security_group" "backend_alb_sg" {
+  name        = "${var.project_name}-backend-alb-sg"
+  description = "Allow HTTP from ECS service to backend ALB"
+  vpc_id      = aws_vpc.project_name_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.ecs_frontend_service_sg.id]
+  }
+
+  egress {
+    description     = "Allow HTTP ALB to ECS backend service"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  depends_on = [aws_vpc.project_name_vpc, aws_cloudfront_distribution.project_name_cdn] #TODO
+}
+
+resource "aws_security_group" "ecs_backend_service_sg" {
+  name        = "${var.project_name}-ecs-backend-service-sg"
+  description = "backend ECS service to RDS"
+  vpc_id      = aws_vpc.project_name_vpc.id
+
+  ingress {
+    description     = "Allow HTTP from private ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.backend_alb_sg.id]
+  }
+
+  egress {
+    description     = "Allow traffic to RDS"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.rds_sg.id]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project_name}-rds-sg"
+  description = "RDS security group"
+  vpc_id      = aws_vpc.project_name_vpc.id
+
+  ingress {
+    description     = "Allow traffic from ECS backend service"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_backend_service_sg.id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
