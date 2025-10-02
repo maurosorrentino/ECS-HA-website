8a73c5fcb6b3190f97aafafed12b9fb070c878a1
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-02 15:31:04 +0100

    terraform fmt

--- terraform-infrastructure/modules/s3/s3.tf
+++ terraform-infrastructure/modules/s3/s3.tf
@@ -14,21 +14,16 @@ resource "aws_s3_bucket_lifecycle_configuration" "project_name_s3_lifecycle" {
   bucket = aws_s3_bucket.project_name_s3.id
 
   rule {
-    id     = "TransitionToGlacier"
+    id     = "TransitionToDeepArchive"
     status = "Enabled"
 
     filter {}
 
     transition {
       days          = 30
-      storage_class = "GLACIER"
+      storage_class = "DEEP_ARCHIVE"
     }
   }
-}
-
-resource "aws_kms_key" "project_name_s3_kms" {
-  description             = "KMS key for S3 log bucket encryption"
-  deletion_window_in_days = 7
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "project_name_s3_encryption" {
@@ -36,7 +31,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "project_name_s3_e
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.project_name_s3_kms.arn
+      kms_master_key_id = var.kms_key_arn
       sse_algorithm     = "aws:kms"
     }
   }
--- terraform-infrastructure/modules/s3/vars.tf
+++ terraform-infrastructure/modules/s3/vars.tf
@@ -1,4 +1,9 @@
 variable "bucket_name" {
-    description = "The name of the S3 bucket to be encrypted"
-    type        = string
+  description = "The name of the S3 bucket to be encrypted"
+  type        = string
+}
+
+variable "kms_key_arn" {
+  description = "The ARN of the KMS key to be used for S3 bucket encryption"
+  type        = string
 }
