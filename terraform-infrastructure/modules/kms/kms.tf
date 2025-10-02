4e06230aac1d32de6a7324527717bac40ff22331
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-02 15:30:47 +0100

    terraform fmt

--- terraform-infrastructure/modules/kms/kms.tf
+++ terraform-infrastructure/modules/kms/kms.tf
@@ -1,19 +1,20 @@
 resource "aws_kms_key" "project_name_s3_bucket_key" {
-    description             = "KMS key for S3 ${var.bucket_name}"
+  description         = "KMS key for S3 ${var.bucket_name}"
+  enable_key_rotation = true # automatic rotation every year
 }
 
 resource "aws_kms_alias" "project_name_s3_bucket_key_alias" {
-    name          = "alias/${var.bucket_key_alias}"
-    target_key_id = aws_kms_key.project_name_s3_bucket_key.id
+  name          = "alias/${var.bucket_key_alias}"
+  target_key_id = aws_kms_key.project_name_s3_bucket_key.id
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
-    bucket = var.bucket_name
+  bucket = var.bucket_name
 
-    rule {
-        apply_server_side_encryption_by_default {
-            kms_master_key_id = aws_kms_key.project_name_s3_bucket_key.arn
-            sse_algorithm     = "aws:kms"
-        }
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.project_name_s3_bucket_key.arn
+      sse_algorithm     = "aws:kms"
     }
+  }
 }
--- terraform-infrastructure/modules/kms/vars.tf
+++ terraform-infrastructure/modules/kms/vars.tf
@@ -1,9 +1,9 @@
 variable "bucket_name" {
-    description = "The name of the S3 bucket to be encrypted"
-    type        = string
+  description = "The name of the S3 bucket to be encrypted"
+  type        = string
 }
 
 variable "bucket_key_alias" {
-    description = "The alias for the KMS key"
-    type        = string
+  description = "The alias for the KMS key"
+  type        = string
 }
