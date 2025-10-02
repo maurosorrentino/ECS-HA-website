10e8727f4749c9fa1e5b97e8231c96289531b7d5
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-02 16:08:32 +0100

    changed subnet to frontend alb and created rds instance

--- terraform-infrastructure/rds.tf
+++ terraform-infrastructure/rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "project_name_rds_instance" {
   identifier             = "${var.project_name}-rds-instance"
   engine                 = "mysql"
   engine_version         = "8.0"
-  instance_class         = "db.t3.micro"
+  instance_class         = "db.t3.micro" # free tier
   allocated_storage      = 20
   storage_type           = "gp2"
   username               = "admin"
