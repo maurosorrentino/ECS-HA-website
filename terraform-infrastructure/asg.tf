3efcecc999da20bb74711a089d600086f3257deb
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-06 16:37:56 +0100

    undecleared resource fix

--- terraform-infrastructure/asg.tf
+++ terraform-infrastructure/asg.tf
@@ -3,7 +3,7 @@ module "name_project_ecs_frontend_asg" {
   asg_name           = "${var.project_name}-ecs-frontend-asg"
   private_subnet_ids = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("frontend", name))]
   template_id        = module.project_name_frontend_ecs_service.launch_template_id
-  target_group_arns  = [module.project_name_frontend_alb.alb_target_group_arn]
+  target_group_arns  = [module.project_name_frontend_alb.target_group_arn]
 
   depends_on = [module.project_name_frontend_alb, aws_subnet.project_name_private_subnets]
 }
@@ -13,7 +13,7 @@ module "name_project_ecs_backend_asg" {
   asg_name           = "${var.project_name}-ecs-backend-asg"
   private_subnet_ids = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("backend", name))]
   template_id        = module.project_name_backend_ecs_service.launch_template_id
-  target_group_arns  = [module.project_name_backend_alb.alb_target_group_arn]
+  target_group_arns  = [module.project_name_backend_alb.target_group_arn]
 
   depends_on = [module.project_name_backend_alb, aws_subnet.project_name_private_subnets]
 }
