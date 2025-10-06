a05733f65d67f8c769ac9ce9188e5ffb9c2e20d2
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-06 11:41:38 +0100

    service log groups

---
+++ terraform-infrastructure/log_group.tf
@@ -0,0 +1,11 @@
+resource "aws_cloudwatch_log_group" "project_name_frontend_service_log_group" {
+  name = "${var.project_name}-frontend-service-log-group"
+  retention_in_days = 365
+}
+
+resource "aws_cloudwatch_log_group" "project_name_backend_service_log_group" {
+  name = "${var.project_name}-backend-service-log-group"
+  retention_in_days = 365
+}
+
+# TODO kms
