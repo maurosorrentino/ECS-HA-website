93c6b3942109e98a243e90ed0479443993778b68
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-08 11:59:33 +0100

    put var.account_id in kms modules (policy)

--- terraform-infrastructure/modules/kms/cw_log_group/kms.tf
+++ terraform-infrastructure/modules/kms/cw_log_group/kms.tf
@@ -11,9 +11,9 @@ resource "aws_kms_key" "project_name_cloudwatch_logs" {
         "Principal": {
           "AWS": [
             # need for terraform destroy, do not use in prod
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root", 
+            "arn:aws:iam::${var.account_id}:root", 
             # allow users with a specific role to decrypt logs
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devGitHubActionsProjectNameRole" 
+            "arn:aws:iam::${var.account_id}:role/devGitHubActionsProjectNameRole" 
           ]
         },
         # in prod limit the actions to what is strictly necessary
