b20f60733f53c1e6c67d6cb03a03b6e3a50c3cc1
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-08 11:32:25 +0100

    changed kms policy

--- terraform-infrastructure/modules/kms/cw_log_group/kms.tf
+++ terraform-infrastructure/modules/kms/cw_log_group/kms.tf
@@ -2,8 +2,41 @@ resource "aws_kms_key" "project_name_cloudwatch_logs" {
   description             = "CloudWatch Logs encryption key"
   enable_key_rotation     = true
 
-  policy = templatefile("${path.root}/modules/kms/kms_policy.json", {
-    account_id = var.account_id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "AllowRootAndAdminAccess",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": [
+            # need for terraform destroy, do not use in prod
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root", 
+            # allow users with a specific role to decrypt logs
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devGitHubActionsProjectNameRole" 
+          ]
+        },
+        # in prod limit the actions to what is strictly necessary
+        # I want everything for destroying the stack easily
+        "Action": "kms:*",
+        "Resource": "*"
+      },
+      {
+        "Sid": "AllowCloudWatchLogsUse",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "logs.eu-west-2.amazonaws.com"
+        },
+        "Action": [
+          "kms:Encrypt*",
+          "kms:Decrypt*",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        "Resource": "*"
+      }
+    ]
   })
 }
 
--- terraform-infrastructure/modules/kms/s3/kms.tf
+++ terraform-infrastructure/modules/kms/s3/kms.tf
@@ -2,8 +2,41 @@ resource "aws_kms_key" "project_name_s3_bucket_key" {
   description         = "KMS key for S3 ${var.bucket_name}"
   enable_key_rotation = true # automatic rotation every year
 
-  policy = templatefile("${path.root}/modules/kms/kms_policy.json", {
-    account_id = var.account_id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "AllowRootAndAdminAccess",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": [
+            # need for terraform destroy, do not use in prod
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root", 
+            # allow users with a specific role to decrypt logs
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devGitHubActionsProjectNameRole" 
+          ]
+        },
+        # in prod limit the actions to what is strictly necessary
+        # I want everything for destroying the stack easily
+        "Action": "kms:*",
+        "Resource": "*"
+      },
+      {
+        "Sid": "AllowS3Use",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "s3.amazonaws.com"
+        },
+        "Action": [
+          "kms:Encrypt*",
+          "kms:Decrypt*",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        "Resource": "*"
+      }
+    ]
   })
 }
 
