6c6c72b6d8c9a38430c9faf7f69aa0123fad199d
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-09 14:31:53 +0100

    s3 was getting wrong kms keys

--- terraform-infrastructure/s3.tf
+++ terraform-infrastructure/s3.tf
@@ -9,7 +9,7 @@ module "project_name_cloudfront_logs_s3" {
 module "project_name_alb_logs_s3" {
   source      = "./modules/s3"
   bucket_name = local.alb_bucket_name
-  kms_key_arn = module.project_name_cloudfront_logs_s3_kms.kms_arn
+  kms_key_arn = module.project_name_alb_logs_s3_kms.kms_arn
 
   depends_on = [module.project_name_alb_logs_s3_kms]
 }
