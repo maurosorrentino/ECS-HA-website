046e128b650e0612b294339f9f0edfb708ff3c98
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-09 14:11:40 +0100

    commented alb to deploy after s3 logs to troubleshoot

--- terraform-infrastructure/alb.tf
+++ terraform-infrastructure/alb.tf
@@ -1,25 +1,25 @@
-module "project_name_frontend_alb" {
-  source                = "./modules/alb"
-  alb_name              = "${var.project_name}-frontend-alb"
-  alb_target_group_name = "${var.project_name}-frontend-tg"
-  vpc_id                = aws_vpc.project_name_vpc.id
-  alb_security_groups   = [aws_security_group.project_name_frontend_alb_sg.id]
-  alb_subnets           = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("alb", name))]
-  bucket_id             = module.project_name_alb_logs_s3.bucket_id
-  s3_prefix             = "frontend-alb-logs"
+# module "project_name_frontend_alb" {
+#   source                = "./modules/alb"
+#   alb_name              = "${var.project_name}-frontend-alb"
+#   alb_target_group_name = "${var.project_name}-frontend-tg"
+#   vpc_id                = aws_vpc.project_name_vpc.id
+#   alb_security_groups   = [aws_security_group.project_name_frontend_alb_sg.id]
+#   alb_subnets           = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("alb", name))]
+#   bucket_id             = module.project_name_alb_logs_s3.bucket_id
+#   s3_prefix             = "frontend-alb-logs"
 
-  depends_on = [aws_subnet.project_name_private_subnets, aws_security_group.project_name_frontend_alb_sg, module.project_name_alb_logs_s3]
-}
+#   depends_on = [aws_subnet.project_name_private_subnets, aws_security_group.project_name_frontend_alb_sg, module.project_name_alb_logs_s3]
+# }
 
-module "project_name_backend_alb" {
-  source                = "./modules/alb"
-  alb_name              = "${var.project_name}-backend-alb"
-  alb_target_group_name = "${var.project_name}-backend-tg"
-  vpc_id                = aws_vpc.project_name_vpc.id
-  alb_security_groups   = [aws_security_group.project_name_backend_alb_sg.id]
-  alb_subnets           = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("alb", name))]
-  bucket_id             = module.project_name_alb_logs_s3.bucket_id
-  s3_prefix             = "backend-alb-logs"
+# module "project_name_backend_alb" {
+#   source                = "./modules/alb"
+#   alb_name              = "${var.project_name}-backend-alb"
+#   alb_target_group_name = "${var.project_name}-backend-tg"
+#   vpc_id                = aws_vpc.project_name_vpc.id
+#   alb_security_groups   = [aws_security_group.project_name_backend_alb_sg.id]
+#   alb_subnets           = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("alb", name))]
+#   bucket_id             = module.project_name_alb_logs_s3.bucket_id
+#   s3_prefix             = "backend-alb-logs"
 
-  depends_on = [aws_subnet.project_name_private_subnets, aws_security_group.project_name_backend_alb_sg, module.project_name_alb_logs_s3]
-}
+#   depends_on = [aws_subnet.project_name_private_subnets, aws_security_group.project_name_backend_alb_sg, module.project_name_alb_logs_s3]
+# }
