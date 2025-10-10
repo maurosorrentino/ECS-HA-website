8f00e028d0041b461f84e921ccfb498d98501bc4
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-10 09:19:33 +0100

    s3 alb logs policy changed

--- terraform-infrastructure/s3_iam.tf
+++ terraform-infrastructure/s3_iam.tf
@@ -11,7 +11,8 @@ resource "aws_s3_bucket_policy" "alb_logs_policy" {
           Service : "elasticloadbalancing.amazonaws.com"
         },
         Action : "s3:PutObject",
-        Resource : "arn:aws:s3:::${module.project_name_alb_logs_s3.bucket_id}/AWSLogs/*",
+        # try first with /* and then s3_prefix
+        Resource : "arn:aws:s3:::${module.project_name_alb_logs_s3.bucket_id}/*",
         Condition : {
           StringEquals : {
             "aws:SourceAccount" : data.aws_caller_identity.current.account_id
