3f648001957646495cef346568f408c79f3bef19
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-10 10:37:40 +0100

    kms encryption on alb logs is not supported, only nlb

--- terraform-infrastructure/s3_iam.tf
+++ terraform-infrastructure/s3_iam.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_policy" "alb_logs_policy" {
             ]
           }
         }
-      # },
+      },
       # {
       #   Sid = "AllowALBGetBucketAcl",
       #   Effect = "Allow",
