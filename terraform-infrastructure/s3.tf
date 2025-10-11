adebdcabc9698afdcfd64ca2c993bc28d76326ad
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-11 13:35:12 +0100

    cloudfront s3 has policy again

--- terraform-infrastructure/s3.tf
+++ terraform-infrastructure/s3.tf
@@ -7,17 +7,3 @@ module "project_name_alb_logs_s3" {
   source      = "./modules/s3"
   bucket_name = "${var.project_name}-alb-logs"
 }
-
-# cloudfront still depends on ACLs instead of Bucket policies
-resource "aws_s3_bucket_ownership_controls" "cloudfront_logs" {
-  bucket = module.project_name_cloudfront_logs_s3.bucket_id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "cloudfront_logs" {
-  bucket = module.project_name_cloudfront_logs_s3.bucket_id
-  acl    = "log-delivery-write"
-}
