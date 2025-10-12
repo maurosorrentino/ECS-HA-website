3e8c989fac3f399ffc7144b3febe1f6e6983c3c8
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-12 11:07:58 +0100

    pipeline changed to use env variables, needs testing

--- terraform-infrastructure/backend.tf
+++ terraform-infrastructure/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-    # bucket key and region is in the terraform init command and env variable are in .github/workflows/ENV/env_vars.json
+    # bucket key and region is in the terraform init command and 
+    # env variable are in .github/workflows/ENV/dev or prod/env_vars.json
     encrypt        = true
   }
 }
