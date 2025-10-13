1da0afaa1e0316028e396c6fa3f8458ab463243e
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-13 13:23:42 +0100

    changed asg desidered capacity

--- terraform-infrastructure/modules/asg/asg.tf
+++ terraform-infrastructure/modules/asg/asg.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_group" "project_name_ecs_asg" {
   name                = var.asg_name
   max_size            = 1 # free tier, change as you need
   min_size            = 1 # free tier, change as you need
-  desired_capacity    = 0 # free tier, change as you need
+  desired_capacity    = 1 # free tier, change as you need
   target_group_arns   = var.target_group_arns
   vpc_zone_identifier = var.private_subnet_ids
 
