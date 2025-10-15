2d4830831e12ea2f28b7458ace27b642565c16d0
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-15 16:49:16 +0100

    3 nat gateway, needs testing

--- terraform-infrastructure/ngw.tf
+++ terraform-infrastructure/ngw.tf
@@ -1,12 +1,19 @@
 resource "aws_eip" "project_name_elastic_ip" {
+  for_each = aws_subnet.project_name_public_subnets
   domain = "vpc"
+  
+  tags = { 
+    Name = "${var.project_name}-elastic-ip-${each.key}"
+  }
+
 }
 
 resource "aws_nat_gateway" "project_name_nat_gateway" {
   for_each      = aws_subnet.project_name_public_subnets
-  allocation_id = aws_eip.project_name_elastic_ip.id
+  allocation_id = aws_eip.project_name_elastic_ip[each.key].id
   subnet_id     = each.value.id
-  tags = {
-    Name = "${var.project_name}-nat-gateway"
+  tags = { 
+    Name = "${var.project_name}-nat-gateway-${each.key}" 
   }
+
 }
