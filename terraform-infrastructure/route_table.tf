1a045a2558482d025d21395f2ea3a697929b5dc5
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-16 11:40:28 +0100

    3 nat gateway + 3 route tables, needs testing

--- terraform-infrastructure/route_table.tf
+++ terraform-infrastructure/route_table.tf
@@ -40,7 +40,7 @@ resource "aws_route_table_association" "private" {
   for_each = aws_subnet.project_name_private_subnets
 
   subnet_id      = each.value.id
-  # Looks up the private route table associated with the NAT gateway for the corresponding AZ
+  # looks up the private route table associated with the NAT gateway for the corresponding AZ
   route_table_id = aws_route_table.private["${var.project_name}-public-subnet-${regex("[a-c]$", each.key)}"].id
 
   depends_on = [aws_subnet.project_name_private_subnets]
