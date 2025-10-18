5750d9f43c4bc32ca5e795ada7ba4d033ea63dc6
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-18 19:48:17 +0100

    dynamo lock state table in it's own file

--- terraform-bootstrap/s3.tf
+++ terraform-bootstrap/s3.tf
@@ -1,14 +1,3 @@
 resource "aws_s3_bucket" "terraform_state" {
     bucket = "terraform-state-department-name"
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-    name         = "terraform-lock-table"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key     = "LockID"
-
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
-}
