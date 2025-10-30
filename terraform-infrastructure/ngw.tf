6781e371f8b972bee33b2a424fc1258a0728f112
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-30 10:58:29 +0000

    pipeline bug fix

--- terraform-infrastructure/ngw.tf
+++ terraform-infrastructure/ngw.tf
@@ -1,5 +1,6 @@
 resource "aws_eip" "project_name_elastic_ip" {
   for_each = aws_subnet.project_name_public_subnets
+
   domain   = "vpc"
 
   tags = {
@@ -10,6 +11,7 @@ resource "aws_eip" "project_name_elastic_ip" {
 
 resource "aws_nat_gateway" "project_name_nat_gateway" {
   for_each      = aws_subnet.project_name_public_subnets
+
   allocation_id = aws_eip.project_name_elastic_ip[each.key].id
   subnet_id     = each.value.id
 
