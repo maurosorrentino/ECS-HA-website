d2ccbca8769ff7bcef0ac573813144a79195cc53
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-11-02 15:39:53 +0000

    terraform fmt

--- terraform-infrastructure/ngw.tf
+++ terraform-infrastructure/ngw.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "project_name_elastic_ip" {
   for_each = aws_subnet.project_name_public_subnets
 
-  domain   = "vpc"
+  domain = "vpc"
 
   tags = {
     Name = "${var.project_name}-elastic-ip-${each.key}"
@@ -10,7 +10,7 @@ resource "aws_eip" "project_name_elastic_ip" {
 }
 
 resource "aws_nat_gateway" "project_name_nat_gateway" {
-  for_each      = aws_subnet.project_name_public_subnets
+  for_each = aws_subnet.project_name_public_subnets
 
   allocation_id = aws_eip.project_name_elastic_ip[each.key].id
   subnet_id     = each.value.id
--- terraform-infrastructure/provider.tf
+++ terraform-infrastructure/provider.tf
@@ -20,4 +20,3 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
