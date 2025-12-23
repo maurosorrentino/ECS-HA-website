d831da9d4b74e32d9be9639eb40cc966870ea424
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-12-23 11:09:14 +0100

    MVP app + docker compose

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 
 function App() {
 
+  const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
   const callApi = async () => {
     try {
       const payload = {
         content: "Hello from React"
       };
 
-      const response = await fetch('/api/put-into-db', {
+      const response = await fetch(`${BASE_URL}/api/put-into-db`, {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
       });
 
@@ -32,11 +32,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
         <button onClick={callApi}>
           Insert into DB
         </button>
-
       </header>
     </div>
   );
