c87b8fe2d7b2bb9ca5cc7738e732d43173eb79d3
maurosorrentino <sorrentino.mauro95@gmail.com>
2026-02-06 11:39:49 +0000

    removed unused vars

--- terraform-ecs-image-update/vars.tf
+++ terraform-ecs-image-update/vars.tf
@@ -3,11 +3,6 @@ variable "frontend_image_uri" {
   default = "FRONTEND_IMAGE"
 }
 
-# TODO add env variable
-variable "task_definition_frontend_name" {
-  type = string
-}
-
 variable "service_frontend_name" {
   type    = string
   default = "frontend-service"
@@ -16,11 +11,6 @@ variable "service_frontend_name" {
 variable "backend_image_uri" {
   type    = string
   default = "BACKEND_IMAGE"
-}
-
-# TODO add env variable
-variable "task_definition_backend_name" {
-  type = string
 }
 
 variable "service_backend_name" {
