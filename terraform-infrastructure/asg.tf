d44851c12fa420bfdd4f26b1a98238e2b87548a6
maurosorrentino <sorrentino.mauro95@gmail.com>
2026-02-06 16:40:07 +0000

    moved launch template into asg

--- terraform-infrastructure/asg.tf
+++ terraform-infrastructure/asg.tf
@@ -2,7 +2,6 @@ module "name_project_ecs_frontend_asg" {
   source             = "./modules/asg"
   asg_name           = "${var.project_name}-ecs-frontend-asg"
   private_subnet_ids = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("frontend", name))]
-  template_id        = module.project_name_frontend_ecs_service.launch_template_id
   launch_template_name_prefix         = "${var.project_name}-ecs-frontend-"
   instance_profile_name               = aws_iam_instance_profile.project_name_ecs_frontend_profile.name
   launch_template_security_groups_ids = [aws_security_group.project_name_ecs_frontend_service_sg.id]
@@ -15,7 +14,6 @@ module "name_project_ecs_backend_asg" {
   source             = "./modules/asg"
   asg_name           = "${var.project_name}-ecs-backend-asg"
   private_subnet_ids = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("backend", name))]
-  template_id        = module.project_name_backend_ecs_service.launch_template_id
   launch_template_name_prefix         = "${var.project_name}-ecs-backend-"
   instance_profile_name               = aws_iam_instance_profile.project_name_ecs_backend_profile.name
   launch_template_security_groups_ids = [aws_security_group.project_name_ecs_backend_service_sg.id]
--- terraform-infrastructure/modules/asg/asg.tf
+++ terraform-infrastructure/modules/asg/asg.tf
@@ -1,33 +1,3 @@
-resource "aws_autoscaling_group" "project_name_ecs_asg" {
-  name                = var.asg_name
-  max_size            = 1 # free tier, change as you need
-  min_size            = 1 # free tier, change as you need
-  desired_capacity    = 1 # free tier, change as you need
-  vpc_zone_identifier = var.private_subnet_ids
-
-  launch_template {
-    id      = var.template_id
-    version = "$Latest"
-  }
-
-  health_check_type         = "EC2"
-  health_check_grace_period = 300
-}
-
-resource "aws_autoscaling_policy" "project_name_asg_cpu_target" {
-  name                   = "${var.asg_name}-cpu-scaling"
-  autoscaling_group_name = aws_autoscaling_group.project_name_ecs_asg.name
-  policy_type            = "TargetTrackingScaling"
-
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
-
-    target_value = 80
-  }
-}
-
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
@@ -58,4 +28,34 @@ resource "aws_launch_template" "project_name_ecs_lt" {
               echo ECS_CLUSTER=${var.cluster_name} >> /etc/ecs/ecs.config
               EOT
   )
+}
+
+resource "aws_autoscaling_group" "project_name_ecs_asg" {
+  name                = var.asg_name
+  max_size            = 1 # free tier, change as you need
+  min_size            = 1 # free tier, change as you need
+  desired_capacity    = 1 # free tier, change as you need
+  vpc_zone_identifier = var.private_subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.project_name_ecs_lt.id
+    version = "$Latest"
+  }
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+}
+
+resource "aws_autoscaling_policy" "project_name_asg_cpu_target" {
+  name                   = "${var.asg_name}-cpu-scaling"
+  autoscaling_group_name = aws_autoscaling_group.project_name_ecs_asg.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 80
+  }
 }
