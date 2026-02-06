aa5ae5641d81edbe935e42259ac09c7b80c7ea9b
maurosorrentino <sorrentino.mauro95@gmail.com>
2026-02-06 12:23:58 +0000

    changed name of var

--- terraform-ecs-image-update/ecs.tf
+++ terraform-ecs-image-update/ecs.tf
@@ -1,13 +1,13 @@
 module "update_frontend" {
   source       = "./modules"
-  service_name = var.service_frontend_name
+  service_name = var.frontend_service_name
   cluster_name = var.cluster_name
   image_uri    = var.frontend_image_uri
 }
 
 module "update_backend" {
   source       = "./modules"
-  service_name = var.service_backend_name
+  service_name = var.backend_service_name
   cluster_name = var.cluster_name
   image_uri    = var.backend_image_uri
 }
