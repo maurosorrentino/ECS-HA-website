cca53f18ef79104d86669e134900188cd61a1e95
maurosorrentino <sorrentino.mauro95@gmail.com>
2026-02-06 11:54:47 +0000

    removed defaults

--- terraform-ecs-image-update/vars.tf
+++ terraform-ecs-image-update/vars.tf
@@ -1,21 +1,17 @@
 variable "frontend_image_uri" {
   type    = string
-  default = "FRONTEND_IMAGE"
 }
 
 variable "service_frontend_name" {
   type    = string
-  default = "frontend-service"
 }
 
 variable "backend_image_uri" {
   type    = string
-  default = "BACKEND_IMAGE"
 }
 
 variable "service_backend_name" {
   type    = string
-  default = "backend-service"
 }
 
 variable "cluster_name" {
@@ -24,11 +20,9 @@ variable "cluster_name" {
 
 variable "project_name" {
   type    = string
-  default = "project-name"
 }
 
 variable "region" {
   type    = string
-  default = "eu-west-2"
 }
 
--- terraform-infrastructure/vars.tf
+++ terraform-infrastructure/vars.tf
@@ -2,13 +2,11 @@
 variable "region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "eu-west-2"
 }
 
 variable "project_name" {
   description = "The name of the project"
   type        = string
-  default     = "project-name"
 }
 
 variable "frontend_service_name" {
