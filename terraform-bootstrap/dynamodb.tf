5c700d1f6679e3038396b7585dadc6ba913ee50b
maurosorrentino <sorrentino.mauro95@gmail.com>
2026-02-08 12:47:37 +0000

    1 state file since app pipeline now uses bash

--- terraform-bootstrap/dynamodb.tf
+++ terraform-bootstrap/dynamodb.tf
@@ -1,16 +1,5 @@
-resource "aws_dynamodb_table" "terraform_lock_infra" {
-    name         = "terraform-infra-lock-table"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key     = "LockID"
-
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
-}
-
-resource "aws_dynamodb_table" "terraform_lock_app" {
-    name         = "terraform-app-lock-table"
+resource "aws_dynamodb_table" "terraform_lock" {
+    name         = "terraform-lock-table"
     billing_mode = "PAY_PER_REQUEST"
     hash_key     = "LockID"
 
