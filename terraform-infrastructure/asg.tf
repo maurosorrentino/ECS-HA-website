b5deff2d8ab22e5ddc22834d42afc3d65d730fcd
maurosorrentino <sorrentino.mauro95@gmail.com>
2026-02-09 13:01:29 +0000

    added permissions

--- terraform-infrastructure/asg.tf
+++ terraform-infrastructure/asg.tf
@@ -4,7 +4,7 @@ module "name_project_ecs_frontend_asg" {
   private_subnet_ids                  = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("frontend", name))]
   launch_template_name_prefix         = "${var.project_name}-ecs-frontend-"
   instance_profile_name               = aws_iam_instance_profile.ecs_instance_profile.name
-  launch_template_security_groups_ids = [aws_security_group.project_name_ecs_frontend_service_sg.id]
+  launch_template_security_groups_ids = [aws_security_group.project_name_ecs_frontend_service_sg.id, aws_security_group.project_name_ecs_task_sg.id]
   cluster_name                        = aws_ecs_cluster.project_name_ecs_cluster.name
 
   depends_on = [module.project_name_frontend_alb, aws_subnet.project_name_private_subnets, aws_ecs_cluster.project_name_ecs_cluster]
@@ -16,7 +16,7 @@ module "name_project_ecs_backend_asg" {
   private_subnet_ids                  = [for name, subnet in aws_subnet.project_name_private_subnets : subnet.id if can(regex("backend", name))]
   launch_template_name_prefix         = "${var.project_name}-ecs-backend-"
   instance_profile_name               = aws_iam_instance_profile.ecs_instance_profile.name
-  launch_template_security_groups_ids = [aws_security_group.project_name_ecs_backend_service_sg.id]
+  launch_template_security_groups_ids = [aws_security_group.project_name_ecs_backend_service_sg.id, aws_security_group.project_name_ecs_task_sg.id]
   cluster_name                        = aws_ecs_cluster.project_name_ecs_cluster.name
 
   depends_on = [module.project_name_backend_alb, aws_subnet.project_name_private_subnets, aws_ecs_cluster.project_name_ecs_cluster]
