03812eb09f61753234829f08050bcaad5b22b821
maurosorrentino <sorrentino.mauro95@gmail.com>
2026-02-09 12:25:43 +0000

    ecs permissions + tf fmt

--- terraform-infrastructure/ecs_iam.tf
+++ terraform-infrastructure/ecs_iam.tf
@@ -98,8 +98,8 @@ resource "aws_iam_role" "frontend_task_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
       Principal = { Service = "ecs-tasks.amazonaws.com" }
     }]
   })
