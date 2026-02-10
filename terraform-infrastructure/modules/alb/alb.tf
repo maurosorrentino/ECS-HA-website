6ecb67be3d6a1eb23d1175c651d02db846b53b5a
maurosorrentino <sorrentino.mauro95@gmail.com>
2026-02-10 13:36:51 +0000

    dynamic listener port

--- terraform-infrastructure/modules/alb/alb.tf
+++ terraform-infrastructure/modules/alb/alb.tf
@@ -17,8 +17,8 @@ resource "aws_lb" "project_name_alb" {
 
 resource "aws_lb_target_group" "project_name_tg" {
   name     = var.alb_target_group_name
-  port     = 443
-  protocol = "HTTPS"
+  port     = 80
+  protocol = "HTTP"
   vpc_id   = var.vpc_id
 
   health_check {
